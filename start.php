18d1afb47098e31f1cf17b58b36adc93a4a77937
Louis <97880966+CMS2023@users.noreply.github.com>
2023-11-25 14:02:05 +0100

    Add files via upload

---
+++ start.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Poetry Slam Nordhessen</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <li><a href="home.php">Startseite</a></li>
+      <li><a href="calendar.php">Terminkalender</a></li>
+      <li><a href="workshops.php">Workshops</a></li>
+      <li><a href="contact.php">Teilnahme & Kontakt</a></li>
+      <li><a href="impressum.php">Impressum</a></li>
+    </ul>
+  </nav>
+  <hr>
+  <main>
+    <div>
+      <p>BANNER: PS NH</p>
+    </div>
+    <div>
+      <p>Text „willkommen” / was wir machen</p>
+    </div>
+    <img src="./img/PSNH.png" alt="Banner Poetry Slam">
+    <div>
+      <p>Text„Was ist PS”</p>
+    </div>
+    <img src="./img/PSNH.png" alt="Banner Poetry Slam">
+  </main>
+
+</body>
+
+</html>
