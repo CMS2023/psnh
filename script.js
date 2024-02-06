7bfbc91491ca7e083c93a1670e9d687aa962c49e
Louis <97880966+CMS2023@users.noreply.github.com>
2024-02-06 17:32:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Smartphone-Menü
 document.getElementById('menu-toggle').addEventListener('click', function() {
   var bars = document.querySelectorAll('.bar');
   for (var i = 0; i < bars.length; i++) {
@@ -12,3 +13,15 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('sliding-menu').classList.add('show');
   }
 }) 
+
+function smoothScrollTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+// Optional: Zeigen Sie den Scroll-Button nur an, wenn der Nutzer gescrollt hat
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollTopButton").style.display = "block";
+  } else {
+    document.getElementById("scrollTopButton").style.display = "none";
+  }
+};
